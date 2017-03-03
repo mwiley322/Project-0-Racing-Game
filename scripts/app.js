9f2e415c4ee9818a54afec085c1c6873c7dd6bc6
Megan Wiley <mwiley322@gmail.com>
2017-03-03 15:52:48 -0800

    Player elements can now move a short distance on keydown

--- scripts/app.js
+++ scripts/app.js
@@ -3,92 +3,51 @@ $(document).ready(function() {
   var $player2 = $('#monster2');
   var gameIsOn = false;
 
-  $('.start').click(function(){
-    if (gameIsOn === true) {
-      alert('Game is on, now turning off game...');
-      gameIsOn = false;
-    } else {
-      alert('Game is off, now turning on game...');
-      gameIsOn = true;
-    }
-    gameState();
-    return gameIsOn;
-  }); // closes restart function
 
+//USE SHIFT TO BEGIN GAME
+  $('body').keydown(function(e){
+    if ( e.which === 32 && !gameIsOn) { //if enter is pressed and game is off
+    e.preventDefault();
+    gameIsOn = !gameIsOn; //game is turned on
+    gameState(); //and we can use the player keys to move the element
+    }  //closes if statement
+  }); // closes keydown function
 
 
 function gameState () {
-  if (gameIsOn === true) {
-    console.log('game is on');
-  } else {
-    console.log('game is off');
-  } //closes if statement
+  $('body').bind('keydown', function(e) {
   // $('body').keydown(function(e){
-  //   if ( e.which == 39 ) {
-  //     e.preventDefault();
-  //     console.log('Player 2 move');
-  //   } else if ( e.which == 18 ) {
-  //     e.preventDefault();
-  //     console.log('Player 1 move');
-  //   } //closes else statement
-  // }); // closes keydown function
+    if ( e.which === 18 && gameIsOn) {
+      e.preventDefault();
+      $('#monster1').animate({left: '20px'});
+    } else if ( e.which === 39 && gameIsOn ) {
+      e.preventDefault();
+      $('#monster2').animate({left: '20px'});
+     } //closes else statement
+   }); // closes keydown function
 } // closes gameState function
 
+$('body').keydown(function(e) {
+  gameState();
+}); // closes keydown function
 
 
-      // countDown();
-
-  // $('body').keydown(function(e){
-  //   if ( e.which == 39 ) {
-  //     e.preventDefault();
-  //     console.log('Player 2 move');
-  //   } else if ( e.which == 18 ) {
-  //     e.preventDefault();
-  //     console.log('Player 1 move');
-  //   }
-  // });
-
+//RESTART GAME WITH RESTART BUTTON
+  $('.restart').click(function(){
+    // if (gameIsOn === false) {
+    //   gameIsOn = !gameIsOn;
+    // }
+    // countDown();
+    gameIsOn = !gameIsOn;
+    gameState();
+    return gameIsOn;
+  }); // closes start function
 
 
-
-// } else if ( e.which == 32 ) {
-//   e.preventDefault();
-//   console.log('Game entered');
 
 // Game constructor used every time new game entered, otherwise saved for score.
 // Game start with method that uses key 32 only
 // Player constructor used once for each player at page initializing with button click to start
 // moves are specific to player.. player 1 = 18 to move, player 2= 39 to move
-
-
-
-  // setInterval(movePlane, 20);
-// var keys = {}
-//
-// $('.gameboard').keydown(function(e) {
-//     keys[e.keyCode] = true;
-// });
-//
-// $(document).keyup(function(e) {
-//     delete keys[e.keyCode];
-// });
-//
-// function movePlayer() {
-//     for (var direction in keys) {
-//         if (!keys.hasOwnProperty(direction)) continue;
-//         if (direction == 37) {
-//             $(".player").animate({left: "-=5"}, 0);
-//         }
-//         if (direction == 38) {
-//             $(".player").animate({top: "-=5"}, 0);
-//         }
-//         if (direction == 39) {
-//             $(".player").animate({left: "+=5"}, 0);
-//         }
-//         if (direction == 40) {
-//             $(".player").animate({top: "+=5"}, 0);
-//         }
-//     }
-// }
 
 }); //closes ready function
