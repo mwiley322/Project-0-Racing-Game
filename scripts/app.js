26f50db833e667941d22acab0d8a94b3b89a3f84
Megan Wiley <mwiley322@gmail.com>
2017-03-03 19:26:10 -0800

    Declare a winner and restart button appears

--- scripts/app.js
+++ scripts/app.js
@@ -1,36 +1,43 @@
 $(document).ready(function() {
-  var $player1 = $('#monster1');
-  var $player2 = $('#monster2');
+  var $player1 = $('#player1');
+  var $player2 = $('#player2');
   var gameIsOn = false;
+  var position1 = 0;
+  var position2 = 0;
 
-
-//USE SHIFT TO BEGIN GAME
-  $('body').keydown(function(e){
+//USE ENTER TO BEGIN GAME
+  $('body').keydown(function start(e){
     if ( e.which === 32 && !gameIsOn) { //if enter is pressed and game is off
-    e.preventDefault();
-    gameIsOn = !gameIsOn; //game is turned on
-    gameState(); //and we can use the player keys to move the element
+      e.preventDefault();
+      gameIsOn = !gameIsOn; //game is turned on
+      movePlayers(); //and we can use the player keys to move the element
     }  //closes if statement
-  }); // closes keydown function
+  }); // closes start keydown function
 
-
-function gameState () {
-  $('body').bind('keydown', function(e) {
-  // $('body').keydown(function(e){
-    if ( e.which === 18 && gameIsOn) {
+  function movePlayers () {
+    $('body').keyup(function(e){
       e.preventDefault();
-      $('#monster1').animate({left: '20px'});
-    } else if ( e.which === 39 && gameIsOn ) {
-      e.preventDefault();
-      $('#monster2').animate({left: '20px'});
-     } //closes else statement
-   }); // closes keydown function
-} // closes gameState function
+      if ( e.which === 39 && gameIsOn ) {
+        position1+=10;
+        $player2.animate({left: '+=10px'}, 0);
+      } //closes if statement
+      if ( e.which === 65 && gameIsOn) {
+        position2+=10;
+        $player1.animate({left: '+=10px'}, 0);
+      } //closes if statement
+      evaluateWin();
+    }); // closes keydown function
+  } // closes movePlayers function
 
-$('body').keydown(function(e) {
-  gameState();
-}); // closes keydown function
-
+function evaluateWin() {
+  if (position2 === 100) {
+    alert('Sully wins!');
+    $('button').removeClass('hidden');
+  } else if (position1 === 100){
+    alert('Mike wins!');
+    $('button').removeClass('hidden');
+  } //closes if statement
+}
 
 //RESTART GAME WITH RESTART BUTTON
   $('.restart').click(function(){
@@ -39,7 +46,7 @@ $('body').keydown(function(e) {
     // }
     // countDown();
     gameIsOn = !gameIsOn;
-    gameState();
+    movePlayers();
     return gameIsOn;
   }); // closes start function
 
